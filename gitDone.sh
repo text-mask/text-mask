42482799cf9bb9a843b988123314b30c4d8f9f9e
M.K. Safi <msafi@msafi.com>
2016-03-20 17:11:25 -0700

    Fix git:done task

--- gitDone.sh
+++
@@ -1,9 +0,0 @@
-branchName=$(git symbolic-ref --short HEAD)
-
-if [ -z "$(git status --porcelain)" ] || [ "$branchName" == "master" ]; then
-  # Working directory clean and we're on master
-  echo "All is good"
-else
-  # Uncommitted changes
-  echo "Working directory is not clean or the current branch is not master"
-fi
