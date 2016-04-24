68ba2d8d0d18efc3bb894a15e09b631f7be75e1c
M.K. Safi <msafi@msafi.com>
2016-04-23 20:46:55 -0700

    Basic setup for Angular 2 directive

--- integrations/angular2/typings/browser.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
--- integrations/angular2/typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/ambient/jasmine/index.d.ts" />
