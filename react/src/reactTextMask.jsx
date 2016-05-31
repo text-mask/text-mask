de401e9e6a302a997e96a01b61a1b395c881cae3
Jason Brown <jbrown@miradorfin.com>
2016-05-31 09:14:27 -0700

    Add ability to change value with `value` prop to prevent wrong internal value.

--- react/src/reactTextMask.jsx
+++ react/src/reactTextMask.jsx
@@ -22,7 +22,8 @@ export const MaskedInput = React.createClass({
     if (
       nextProps.mask !== this.props.mask ||
       nextProps.guide !== this.props.guide ||
-      nextProps.placeholderCharacter !== this.props.placeholderCharacter
+      nextProps.placeholderCharacter !== this.props.placeholderCharacter ||
+      (nextProps.value !== undefined && nextProps.value !== this.state.conformedInput)
     ) {
       const {mask, value: inputValue, guide, placeholderCharacter: placeholderChar} = nextProps
 
