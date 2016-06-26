c8733c0be54c9d87129deea902511481829c8a78
M.K. Safi <msafi@msafi.com>
2016-06-25 20:44:53 -0700

    Add textMask mono publish task to `package.json`

---
+++ publish.sh
@@ -0,0 +1,14 @@
+echo "Expected arguments: 1) version"
+
+version=$1
+
+if [ -z "$version" ]; then
+  echo "version is not set"
+  exit 1
+fi
+
+generatedNpmVersion="$(npm version $version)"
+
+npm publish --access public
+
+git tag "text-mask-${generatedNpmVersion}"
