aa27a78995838096a09dbcb1381161b6a41322bb
M.K. Safi <msafi@msafi.com>
2016-06-25 20:55:25 -0700

    Publish packages

--- publish.sh
+++ publish.sh
@@ -9,6 +9,4 @@ fi
 
 generatedNpmVersion="$(npm version $version)"
 
-npm publish --access public
-
-git tag "text-mask-${generatedNpmVersion}"
+npm publish --access public --tag "text-mask-${generatedNpmVersion}"
