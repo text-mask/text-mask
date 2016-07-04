2a4295cf477f6aac3d007205d1be2abb3f453d3e
M.K. Safi <msafi@msafi.com>
2016-07-03 19:42:36 -0700

    no message

--- addons/src/createCurrencyMask.js
+++ addons/src/createCurrencyMask.js
@@ -1,13 +1,17 @@
-export default function createCurrencyMask() {
+export default function createCurrencyMask({
+  prefix = '$',
+  suffix = ''
+} = {}) {
   return function(userInput) {
     userInput = userInput.replace(/\D+/g, '')
     userInput = userInput.replace(/\d/g, '1')
 
-    return `$${numberWithCommas(userInput)}`
+    return `${prefix}${numberWithCommas(userInput)}${suffix}`
   }
 }
 
 function numberWithCommas(n) {
   var parts = n.toString().split(".");
+
   return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
 }
--- common/dynamicMaskTests.js
+++ common/dynamicMaskTests.js
@@ -1,4 +1,8 @@
-const dynamicMaskTests = [{
+import _ from 'lodash/fp'
+
+// export default _.filter((t) => false, [{
+// export default _.filter((t) => t, [{
+export default _.filter((t) => t.only, [{
   input: {
     startingInputFieldValue: '$100',
     userModifiedInputFieldValue: '$1000',
@@ -10,6 +14,32 @@ const dynamicMaskTests = [{
   output: {
     adjustedCaretPosition: 6,
   },
-}]
+}, {
+  input: {
+    startingInputFieldValue: '2 $',
+    userModifiedInputFieldValue: '24 $',
+    mask: '11 $',
+    caretPositionAfterInputFieldValueChange: 2,
+    conformedInputFieldValue: '24 $',
+  },
 
-export default dynamicMaskTests
+  output: {
+    adjustedCaretPosition: 2,
+  },
+
+  // only: true,
+}, {
+  input: {
+    startingInputFieldValue: '$200.34',
+    userModifiedInputFieldValue: '$2003.34',
+    mask: '$1,111.11',
+    caretPositionAfterInputFieldValueChange: 6,
+    conformedInputFieldValue: '$2,003.34',
+  },
+
+  output: {
+    adjustedCaretPosition: 6,
+  },
+
+  only: true
+}])
