0f45f9c343864b9ca197c0e0782b7db197e205fc
sky-coding <admin@sky-coding.com>
2016-07-22 17:57:39 -0700

    Angular 2: mask initial value (#110)

--- angular2/src/angular2TextMask.ts
+++ angular2/src/angular2TextMask.ts
@@ -9,8 +9,8 @@ import createTextMaskInputElement from '../../core/src/createTextMaskInputElemen
   }
 })
 export default class MaskedInputDirective {
-  private inputElement:HTMLInputElement
-  public control: any
+  private inputElement:HTMLInputElement;
+  public control: any;
 
   @Input('textMask') textMaskConfig = {
     mask: '',
@@ -20,26 +20,25 @@ export default class MaskedInputDirective {
     keepCharPositions: false,
     onReject: undefined,
     onAccept: undefined
-  }
+  };
 
   constructor(inputElement: ElementRef, private ngControl: NgControl) {
     this.inputElement = inputElement.nativeElement
   }
 
   ngOnInit() {
-    const {placeholderChar} = this.textMaskConfig
-
+    const {placeholderChar} = this.textMaskConfig;
     this.control = createTextMaskInputElement(Object.assign({
       inputElement: this.inputElement,
       placeholderChar,
-    }, this.textMaskConfig))
+    }, this.textMaskConfig));
 
-    this.control.update()
+    setTimeout(() => this.onInput());
   }
 
   onInput() {
-    this.control.update()
-    this.ngControl.viewToModelUpdate(this.inputElement.value)
+    this.control.update();
+    this.ngControl.viewToModelUpdate(this.inputElement.value);
   }
 }
 
