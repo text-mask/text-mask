c3a1ca4ee2014a132dc65cb40413ec5be9f8959c
Loz Jackson <lozjackson@talktalk.net>
2016-08-21 12:37:05 +0100

    update masked-input component

--- ember/addon/components/masked-input.js
+++ ember/addon/components/masked-input.js
@@ -1,6 +1,48 @@
 import Ember from 'ember';
 import { createTextMaskInputElement } from 'ember-text-mask';
 
+/*
+
+  ## MaskedInputComponent
+
+  ### Basic use
+
+  Use the component in templates like this
+
+  ```
+  {{masked-input}}
+  ```
+
+  ### Mask
+
+  define a mask in the template's controller.
+
+  controller.js
+  ```
+  import Ember from 'ember';
+
+  export default Ember.Controller.extend({
+
+    phoneNumberMask: ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
+
+  });
+  ```
+
+  ... then pass the mask to the component in the template
+
+  template.hbs
+  ```
+  {{masked-input mask=phoneNumberMask}}
+  ```
+
+  ### Value
+
+  set the value of the input like any other input element.
+
+  ```
+  {{masked-input value=model.value}}
+  ```
+*/
 export default Ember.TextField.extend({
 
   inputElement: Ember.computed.readOnly('element'),
