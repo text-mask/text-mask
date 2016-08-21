9ac2f794d09f798b3ebae66f3153011d97bd74b7
Loz Jackson <lozjackson@talktalk.net>
2016-08-21 21:37:50 +0100

    ember-text-mask addon (#152)

---
+++ ember/ember-cli-build.js
@@ -0,0 +1,19 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function(defaults) {
+  var app = new EmberAddon(defaults, {
+    snippetPaths: ['tests/dummy/snippets'],
+    snippetSearchPaths: ['tests/dummy/app']
+  });
+
+  /*
+    This build file specifies the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+
+  return app.toTree();
+};
