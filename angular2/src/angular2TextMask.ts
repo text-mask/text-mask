9d9fb9cb6ab71ce3a3d85c875c8e8f8b8fda9390
Ahmed Fouad <KT@Ahmeds-MacBook-Pro.local>
2016-08-26 16:28:08 +0500

    Added support for usage with angular forms.
    Bug Fixes
    1. If input field empty, showing empty text mask value in angular forms. e.g (___) ___-____
    2. Adding trailing _  in form value when attached with angular forms e.g (111) 111-1111_

--- angular2/src/angular2TextMask.ts
+++ angular2/src/angular2TextMask.ts
@@ -1,16 +1,31 @@
-import {Directive, ElementRef, Input} from '@angular/core'
-import {NgControl} from '@angular/forms'
+import { 
+  Directive,
+  ElementRef,
+  Input,
+  Provider,
+  forwardRef
+} from '@angular/core'
+import { NgControl, NG_VALUE_ACCESSOR } from '@angular/forms'
 import createTextMaskInputElement from '../../core/src/createTextMaskInputElement'
 
+const INPUT_VALUE_ACCESSOR = new Provider(NG_VALUE_ACCESSOR, {
+  useExisting: forwardRef(() => MaskedInputDirective),
+  multi: true
+})
+
 @Directive({
+  providers:[INPUT_VALUE_ACCESSOR],
   host: {
     '(input)': 'onInput()'
   },
-  selector: 'input[textMask]'
+  selector: 'input[textMask]',
 })
 export default class MaskedInputDirective {
   private textMaskInputElement: any
   private inputElement:HTMLInputElement
+  private value : string
+  private propagateChange 
+  private propagateTouched
 
   @Input('textMask')
   textMaskConfig = {
@@ -23,20 +38,31 @@ export default class MaskedInputDirective {
     onAccept: undefined
   }
 
-  constructor(inputElement: ElementRef, private ngControl: NgControl) {
+  constructor(inputElement: ElementRef) {
     this.inputElement = inputElement.nativeElement
   }
 
-  ngOnInit() {
-    this.textMaskInputElement = createTextMaskInputElement(Object.assign({inputElement: this.inputElement, }, this.textMaskConfig))
+  writeValue(value) {
+    this.value = value
+  }
 
-    // This ensures that initial model value gets masked
-    setTimeout(() => this.onInput())
+  registerOnChange(fn) {
+    this.propagateChange =  fn
+  }
+
+  registerOnTouched(fn) {
+    this.propagateTouched = fn
+  }
+
+  ngAfterContentInit() {
+    this.textMaskInputElement = createTextMaskInputElement(Object.assign({inputElement: this.inputElement, }, this.textMaskConfig))
+    this.textMaskInputElement.update(this.value)
+    this.propagateChange(this.inputElement.value)
   }
 
   onInput() {
     this.textMaskInputElement.update()
-    this.ngControl.viewToModelUpdate(this.inputElement.value)
+    this.propagateChange(this.inputElement.value)
   }
 }
 
