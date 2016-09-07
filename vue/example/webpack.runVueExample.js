13ef84bcedb3800a0f63502f4139c6036b3c3f55
M.K. Safi <msafi@msafi.com>
2016-09-06 21:04:08 -0700

    Change folder structure and setup `vue:dev`

--- vue/example/webpack.runVueExample.js
+++ vue/example/webpack.runVueExample.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: path.join(__dirname, './main.js'),
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
