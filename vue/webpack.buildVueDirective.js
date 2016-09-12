ac2a2dcbebda3ea17e876e48295343cff0634dd6
Bill Columbia <bcolumbia@ufl.edu>
2016-09-12 18:14:18 -0400

    Allow building of Vue Directive for publishing to npm

---
+++ vue/webpack.buildVueDirective.js
@@ -0,0 +1,59 @@
+var StatsPlugin = require('stats-webpack-plugin')
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, './src/vueTextMask.js'),
+
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'vueTextMask.js',
+    library: 'vueTextMask',
+    libraryTarget: 'umd'
+  },
+
+  resolve: {
+    extensions: ['', '.vue', '.js']
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    }),
+    new StatsPlugin('stats.json', {
+      chunkModules: true
+    })
+  ],
+
+  externals: [
+    {
+      'vue': {
+        root: 'Vue',
+        commonjs2: 'vue',
+        commonjs: 'vue',
+        amd: 'vue'
+      }
+    }
+  ]
+}
