b74f9849796d612b2574cb614e05a850355b54b2
Ibby Hadeed <ibby93@gmail.com>
2016-10-30 15:01:05 -0400

    chore(angular2): working example with systemjs

--- angular2/example/app.module.js
+++ angular2/example/app.module.js
@@ -11,7 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
-// import {TextMaskModule} from '../src/angular2TextMask'
+// import {TextMaskModule} from '../dist/angular2TextMask'
 var app_component_1 = require('./app.component');
 var AppModule = (function () {
     function AppModule() {
--- angular2/example/systemjs.config.js
+++ angular2/example/systemjs.config.js
@@ -4,9 +4,10 @@
  */
 (function (global) {
   System.config({
+    baseURL: '../',
     paths: {
       // paths serve as alias
-      'npm:': '../node_modules/'
+      'npm:': 'node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -16,18 +17,28 @@
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      'text-mask-core': 'npm:text-mask-core',
+      'angular2-text-mask': 'dist/angular2TextMask.js',
       // other libraries
       'rxjs': 'npm:rxjs'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
-      main: {
-        defaultExtension: 'ts'
+      app: {
+        main: 'main.js',
+        defaultExtension: 'js'
       },
       rxjs: {
+        defaultExtension: 'js'
+      },
+      'text-mask-core': {
+        defaultExtension: 'js'
+      },
+      'angular2-text-mask': {
         defaultExtension: 'js'
       }
     }
