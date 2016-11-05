f11ec58bc45bdd3282b7f28c37e308e251847cd7
M.K. Safi <msafi@msafi.com>
2016-11-05 10:14:02 -0700

    Fix CI tasks

--- angular2/example/app.module.ts
+++ angular2/example/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule}      from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
-import {TextMaskModule} from '../dist/angular2TextMask'
+import {TextMaskModule} from '../src/angular2TextMask'
 import AppComponent from './app.component'
 
 @NgModule({
