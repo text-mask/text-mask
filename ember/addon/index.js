a9cbdedde0f7019f1e1d86e4d8b9a0101d1f0ddb
Loz Jackson <lozjackson@talktalk.net>
2017-01-31 19:35:24 +0000

    update index

--- ember/addon/index.js
+++ ember/addon/index.js
@@ -1,4 +1,6 @@
 import createTextMaskInputElement from 'ember-text-mask/createTextMaskInputElement';
 import conformToMask from 'ember-text-mask/conformToMask';
+import MaskedInputComponent from 'ember-text-mask/components/masked-input';
 
+export default MaskedInputComponent;
 export { createTextMaskInputElement, conformToMask };
