f01655ddf326c613e4ee90a93bbf8f668da10724
Loz Jackson <accounts@audiowall.co.uk>
2017-02-08 02:32:51 +0000

    Make the Ember component reactive (#351)

--- ember/tests/unit/components/masked-input-test.js
+++ ember/tests/unit/components/masked-input-test.js
@@ -1,8 +1,4 @@
-import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
-
-const { run } = Ember;
-const mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 
 moduleForComponent('masked-input', 'Unit | Component | masked input', {
   unit: true
@@ -37,33 +33,10 @@ test('mask defaults to an empty array', function(assert) {
   assert.deepEqual(component.get('mask'), []);
 });
 
-test('inputElement is an alias of element', function(assert) {
-  assert.expect(1);
-
-  var component = this.subject();
-  this.render();
-
-  assert.equal(component.get('inputElement'), component.get('element'));
-});
-
-test('createTextMaskInputElement() method exists', function(assert) {
-  assert.expect(1);
-
-  var component = this.subject();
-  this.render();
-  assert.equal(typeof component.createTextMaskInputElement, 'function');
-  component.createTextMaskInputElement({
-    inputElement: component.get('element'),
-    mask
-  });
-});
-
-test('initTextMaskInputElement() method sets textMaskInputElement property', function(assert) {
+test('textMaskInputElement property is initialized', function(assert) {
   assert.expect(4);
-  var component = this.subject({ didInsertElement: null });
+  var component = this.subject();
   this.render();
-
-  run(() => component.initTextMaskInputElement());
 
   const textMaskInputElement = component.get('textMaskInputElement');
   assert.equal(typeof textMaskInputElement, 'object');
@@ -72,49 +45,27 @@ test('initTextMaskInputElement() method sets textMaskInputElement property', fun
   assert.equal(typeof textMaskInputElement.update, 'function');
 });
 
-test('config is correctly passed to createTextMaskInputElement() method', function(assert) {
-  assert.expect(11);
+test('changing textMaskInputElement calls textMaskInputElement.update()', function(assert) {
+  assert.expect(1);
 
-  const placeholderChar = '_';
-  const pipe = function () { return ''; };
-  const onReject = function () { return ''; };
-  const onAccept = function () { return ''; };
-
-  this.subject({
-    mask,
-    guide: true,
-    placeholderChar,
-    keepCharPositions: true,
-    pipe,
-    onReject,
-    onAccept,
-    createTextMaskInputElement: (config) => {
-      assert.ok(config.inputElement);
-      assert.equal(config.mask, mask);
-      assert.equal(config.guide, true);
-      assert.equal(config.placeholderChar, placeholderChar);
-      assert.deepEqual(config.keepCharPositions, true);
-      assert.deepEqual(config.pipe, pipe);
-      assert.equal(typeof config.pipe, 'function');
-      assert.deepEqual(config.onReject, onReject);
-      assert.equal(typeof config.onReject, 'function');
-      assert.deepEqual(config.onAccept, onAccept);
-      assert.equal(typeof config.onAccept, 'function');
-    }
-  });
+  let component = this.subject();
   this.render();
+
+  component.set('textMaskInputElement', {
+    update: () => assert.ok(true)
+  });
 });
 
 test('update() method calls textMaskInputElement.update()', function(assert) {
   assert.expect(1);
 
-  var component = this.subject({ mask });
+  let component = this.subject({
+    _textMaskInputElementChanged(){},
+    textMaskInputElement: {
+      update: () => assert.ok(true)
+    }
+  });
+
   this.render();
-
-  // stub the textMaskInputElement
-  run(() => component.set('textMaskInputElement', {
-    update: () => assert.ok(true)
-  }));
-
   component.update();
 });
