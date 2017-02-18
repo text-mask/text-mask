5bebf388924e9becaa38fb1b2ff17ff2ebf8a8eb
Loz Jackson <lozjackson@talktalk.net>
2017-02-18 17:07:38 +0000

    add tests

--- ember/tests/integration/components/masked-input-test.js
+++ ember/tests/integration/components/masked-input-test.js
@@ -17,3 +17,11 @@ test('placeholder attribute can be passed in', function(assert) {
   this.set('placeholder', "Some Placeholder Text");
   assert.equal(this.$('input').attr('placeholder'), 'Some Placeholder Text');
 });
+
+test('mask is initialised on first render', function(assert) {
+  this.set('mask', ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]);
+  this.set('value', '1234567890');
+  this.render(hbs`{{masked-input mask=mask value=value}}`);
+  assert.equal(this.$('input')[0].value, '(123) 456-7890');
+  assert.equal(this.get('value'), '(123) 456-7890');
+});
