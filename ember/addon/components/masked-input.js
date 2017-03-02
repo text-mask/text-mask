2381b8ae82e7fa34d87c5813aabbf58145742171
Loz Jackson <lozjackson@talktalk.net>
2017-03-02 00:01:14 +0000

    update masked-input component

--- ember/addon/components/masked-input.js
+++ ember/addon/components/masked-input.js
@@ -33,30 +33,31 @@ function _config(...args) {
 */
 export default TextField.extend({
 
-  /*
-    ## mask {Array|Function|Boolean}
-
-    The mask can be an array, or a function that returns an array.
-
-    You can also set the mask to `false`, or return `false` from a mask function
-    to disable the mask completely.
-  */
   mask: [],
 
   /*
     ## config {Object}
 
-    This is a computed property and will update when any of the dependent properties
-    update.  By default it will read the properties off the component root.
+    This is a computed property and will re-compute when any of the dependent properties
+    update.  By default it will read the properties off the component root, you
+    can pass in attrbutes to the component through the template.
+
+    ```hbs
+    {{masked-input
+      mask=customMask
+      guide=true}}
+    ```
   */
   config: _config('mask', 'guide', 'placeholderChar', 'keepCharPositions', 'pipe'),
 
   /*
     ## textMaskInputElement {Object}
 
-    `textMaskInputElement` is the object that is returned from calling `createTextMaskInputElement`.
-    This is a computed property.  It is dependent on the `config` property and
-    will re-compute whenever the config property changes.
+    `textMaskInputElement` is the object that is returned from calling the
+    `createTextMaskInputElement`. method.
+
+    This is a computed property and will re-compute whenever the `config` property
+    changes.
   */
   textMaskInputElement: computed('config', function () {
     let config = get(this, 'config');
@@ -64,31 +65,13 @@ export default TextField.extend({
     return this.createTextMaskInputElement(config);
   }),
 
-  /*
-    ## createTextMaskInputElement(config)
-
-    This method returns the `textMaskInputElement` object.  It is called when
-    the `textMaskInputElement` property is computed...
-  */
   createTextMaskInputElement,
 
-  /*
-    ## update(rawValue, config)
-
-    This method is called on every key press (and on initial render).
-  */
-  update(/*rawValue, config*/) {
+  update() {
     this.get('textMaskInputElement').update(...arguments);
   },
 
-  // call `update` method when the element is rendered.
-  _didInsertElement: on('didInsertElement', function() {
-    this.update();
-  }),
-
-
-  // call `update` method on input.
-  _input: on('input', function() {
+  _didRender: on('input', 'didRender', function() {
     this.update();
   })
 });
