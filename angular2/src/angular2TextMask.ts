7530fa0ea27d87688095255beb050a07ef1d1d30
Loz Jackson <lozjackson@talktalk.net>
2017-03-03 20:23:11 +0000

    update angular2TextMask

--- angular2/src/angular2TextMask.ts
+++ angular2/src/angular2TextMask.ts
@@ -45,11 +45,12 @@ export class MaskedInputDirective implements ControlValueAccessor {
       this.inputElement = this.element.nativeElement.getElementsByTagName('INPUT')[0]
     }
 
-    if (this.inputElement) {
-      this.textMaskInputElement = createTextMaskInputElement(
-          Object.assign({inputElement: this.inputElement}, this.textMaskConfig)
-      )
-    }
+    this.textMaskInputElement = createTextMaskInputElement()
+  }
+
+  update(value) {
+    this.textMaskConfig.inputElement = this.inputElement
+    this.textMaskInputElement.update(value, this.textMaskConfig)
   }
 
   writeValue(value: any) {
@@ -58,7 +59,7 @@ export class MaskedInputDirective implements ControlValueAccessor {
     }
 
     if (this.textMaskInputElement !== undefined) {
-      this.textMaskInputElement.update(value)
+      this.update(value)
     }
   }
 
@@ -72,8 +73,8 @@ export class MaskedInputDirective implements ControlValueAccessor {
     }
 
     if (this.textMaskInputElement !== undefined) {
-      this.textMaskInputElement.update(value)
       
+      this.update(value)
       // get the updated value
       value = this.inputElement.value
 
