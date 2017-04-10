d809cfbf277c4bb49c62b7f748b10f119f39c8c5
Zach Silveira <zackify@gmail.com>
2017-04-10 10:29:03 -0400

    Update for react 15.5

--- react/src/reactTextMask.js
+++ react/src/reactTextMask.js
@@ -1,7 +1,10 @@
-import React, {PropTypes} from 'react'
-import createTextMaskInputElement from '../../core/src/createTextMaskInputElement'
+import React from 'react';
+import PropTypes from 'prop-types';
+import createReactClass from 'create-react-class';
+import createTextMaskInputElement
+  from '../../core/src/createTextMaskInputElement';
 
-export const MaskedInput = React.createClass({
+export const MaskedInput = createReactClass({
   propTypes: {
     mask: PropTypes.oneOfType([
       PropTypes.array,
@@ -9,64 +12,67 @@ export const MaskedInput = React.createClass({
       PropTypes.bool,
       PropTypes.shape({
         mask: PropTypes.oneOfType([PropTypes.array, PropTypes.func]),
-        pipe: PropTypes.func
-      })
+        pipe: PropTypes.func,
+      }),
     ]).isRequired,
     guide: PropTypes.bool,
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     pipe: PropTypes.func,
     placeholderChar: PropTypes.string,
     keepCharPositions: PropTypes.bool,
-    showMask: PropTypes.bool
+    showMask: PropTypes.bool,
   },
 
   createTextMaskInputElement,
 
   initTextMask() {
-    const {props, props: {value}} = this
+    const { props, props: { value } } = this;
 
-    this.textMaskInputElement = this.createTextMaskInputElement({inputElement: this.inputElement, ...props})
-    this.textMaskInputElement.update(value)
+    this.textMaskInputElement = this.createTextMaskInputElement({
+      inputElement: this.inputElement,
+      ...props,
+    });
+    this.textMaskInputElement.update(value);
   },
 
   componentDidMount() {
-    this.initTextMask()
+    this.initTextMask();
   },
 
   componentDidUpdate() {
-    this.initTextMask()
+    this.initTextMask();
   },
 
   render() {
-    const props = {...this.props}
+    const props = { ...this.props };
 
-    delete props.mask
-    delete props.guide
-    delete props.pipe
-    delete props.placeholderChar
-    delete props.keepCharPositions
-    delete props.value
-    delete props.onChange
-    delete props.showMask
+    delete props.mask;
+    delete props.guide;
+    delete props.pipe;
+    delete props.placeholderChar;
+    delete props.keepCharPositions;
+    delete props.value;
+    delete props.onChange;
+    delete props.showMask;
 
     return (
       <input
         {...props}
         onInput={this.onChange}
         defaultValue={this.props.value}
-        ref={(inputElement) => (this.inputElement = inputElement)}
+        ref={inputElement => this.inputElement = inputElement}
       />
-    )
+    );
   },
 
   onChange(event) {
-    this.textMaskInputElement.update()
+    this.textMaskInputElement.update();
 
     if (typeof this.props.onChange === 'function') {
-      this.props.onChange(event)
+      this.props.onChange(event);
     }
-  }
-})
+  },
+});
 
-export default MaskedInput
-export {default as conformToMask} from '../../core/src/conformToMask.js'
+export default MaskedInput;
+export { default as conformToMask } from '../../core/src/conformToMask.js';
