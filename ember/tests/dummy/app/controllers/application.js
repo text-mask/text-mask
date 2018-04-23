f5df4793b4d188c9bb73c5c13de90cb086817ec5
snewcomer <snewcomer24@gmail.com>
2018-04-23 14:50:59 -0700

    fix small lint issue

--- ember/tests/dummy/app/controllers/application.js
+++ ember/tests/dummy/app/controllers/application.js
@@ -1,9 +1,8 @@
 /* BEGIN-SNIPPET controller-mask-example */
 import Controller from '@ember/controller';
 
+const mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 export default Controller.extend({
-
-  mask: ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
-
+  mask
 });
 /* END-SNIPPET */
