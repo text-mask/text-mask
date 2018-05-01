c604c3e84bd8c24def7ab9f1ecbfe937564745af
Thomas <PerfectPixel@users.noreply.github.com>
2018-05-01 22:45:42 +0200

    update angular text-mask directive (#787)
    * update angular text-mask directive
    
    * fix test

--- angular2/test/angular2TextMask.spec.ts
+++ angular2/test/angular2TextMask.spec.ts
@@ -2,7 +2,7 @@ import { MaskedInputDirective } from '../src/angular2TextMask'
 
 describe('MaskedInputDirective', () => {
   it('should create an instance', () => {
-    const directive = new MaskedInputDirective(null, null)
+    const directive = new MaskedInputDirective(null, null, false)
     expect(directive).toBeTruthy()
   })
 })
