840b70f12ee4dce4092dd66caba51685dc0f4224
CFT-Chris <mail@chrislo.ca>
2018-09-26 20:09:43 -0400

    Programmatic value update fix, stray ending character fix

--- ionic3/test/ionic3TextMask.spec.ts
+++ ionic3/test/ionic3TextMask.spec.ts
@@ -2,7 +2,7 @@ import { IonInputMaskDirective } from '../src/ionic3TextMask'
 
 describe('IonInputMaskDirective', () => {
   it('should create an instance', () => {
-    const directive = new IonInputMaskDirective(null, null, false)
+    const directive = new IonInputMaskDirective(null, null, false, null)
     expect(directive).toBeTruthy()
   })
 })
