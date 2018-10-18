fc6171ddd270aba89f3a6c539b5fd8bc361d7eaf
Matt Gibbs <matt.gibbs@onedatascan.com>
2018-10-18 11:43:21 -0400

    Add typing interface for number mask config

--- addons/dist/textMaskAddons.d.ts
+++ addons/dist/textMaskAddons.d.ts
@@ -1,3 +1,17 @@
+export interface NumberMaskConfig {
+    prefix?: string;
+    suffix?: string;
+    includeThousandsSeparator?: boolean;
+    thousandsSeparatorSymbol?: string;
+    allowDecimal?: boolean;
+    decimalSymbol?: string;
+    decimalLimit?: number;
+    integerLimit?: number;
+    requireDecimal?: boolean;
+    allowNegative?: boolean;
+    allowLeadingZeroes?: boolean;
+}
+
 export function createAutoCorrectedDatePipe(a: any): any
-export function createNumberMask(a: any): any
+export function createNumberMask(a: NumberMaskConfig): any
 export const emailMask:any
