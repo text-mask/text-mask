7d5e0b3d991f78714ea37c3899f76beedee28dc6
themzed <mohammmadreza.ziadzadeh@gmail.com>
2019-10-11 20:04:03 +0330

    add newWebsite boilerplate

---
+++ newWebsite/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './style.scss'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, I'm textMask :)</h1>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ newWebsite/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './components/App'
+
+ReactDom.render(<App />, document.getElementById('root'))
