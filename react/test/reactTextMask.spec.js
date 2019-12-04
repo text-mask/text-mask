a7fb2ff8b7254ae4e09483dc090b41551f381322
Musango Wope <musangow@gmail.com>
2019-12-04 13:14:15 +0200

    Fix react test by changing test-utils reference

--- react/test/reactTextMask.spec.js
+++ react/test/reactTextMask.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactTestUtils from 'react-addons-test-utils'
+import ReactTestUtils from 'react-dom/test-utils'
 import packageJson from '../package.json'
 
 const ReactTextMask = (isVerify()) ?
