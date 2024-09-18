9f7c9a51315489ca4d621d6c7a8f090c21c4d2f7
thaboRach <masipathabo.rachidi@vcontractor.co.za>
2024-09-18 10:07:46 +0200

    init vite library & typescript

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import { resolve } from 'path'
+import react from "@vitejs/plugin-react";
+import dts from 'vite-plugin-dts'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),dts({ include: ['lib'], exclude: ['src'] })],
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: "build",
+    lib:{
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', "react-dom"],
+    }
+  },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+  },
+});
