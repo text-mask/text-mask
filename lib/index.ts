05deb24992a4964fc9b24ceb17f99c7c97d9d849
thaboRach <masipathabo.rachidi@vcontractor.co.za>
2024-10-16 16:19:45 +0200

    fix(exports): expose types

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
 export * from './components/MaskedInput/MaskedInput';
 export { conformToMask } from './core';
 export { createAutoCorrectedDatePipe, emailMask, createNumberMask } from './addons';
+export type { Mask } from './types/Mask';
+export type { Pipe } from './types/Pipe';
+export type { MainConfig } from './types/Config';
